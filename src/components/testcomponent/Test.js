81bcd44c532a3f20349a970764ede543e7d04a80
Palguna1121 <palguna2212@gmail.com>
2023-12-10 11:54:26 +0800

    nyoba

---
+++ src/components/testcomponent/Test.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const TestComponent = (props) => {
+  return <div>Hello {props.name}</div>;
+};
+
+export default TestComponent;
---
+++ src/pages/test.js
@@ -0,0 +1,13 @@
+import React from "react";
+import PropTypes from "prop-types";
+import TestComponent from "@/components/testcomponent/Test";
+
+const Test = () => {
+  return (
+    <>
+      <TestComponent name="palguna" />
+    </>
+  );
+};
+
+export default Test;
